2d59a7f6fc8c04dd7dc6ef0dde55066a729a3b7a
davorchar <danrdorcharchir@hotmail.com>
2024-06-14 17:51:07 -0300

    2do commit

---
+++ Act 2 codigo.vb
@@ -0,0 +1,27 @@
+﻿
+
+Public Class Form1
+    Dim dato1 As Integer
+    Dim unidad As Integer
+    Dim decena As Integer
+    Dim centena As Integer
+    Dim resto As Integer
+    Private Sub txtNum_TextChanged(sender As Object, e As EventArgs) Handles txtNum.TextChanged
+        If Integer.TryParse(txtNum.Text, dato1) Then
+        End If
+    End Sub
+
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+
+
+        centena = dato1 \ 100
+        resto = dato1 Mod 100
+        decena = resto \ 10
+        unidad = resto Mod 10
+
+        lblCent.Text = "la centena es" & centena
+        lblDec.Text = "la decena es " & decena
+        lblUnid.Text = "la uniad es" & unidad
+
+    End Sub
+End Class
